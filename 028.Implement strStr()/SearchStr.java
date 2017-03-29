bc573f3ba6fd78037ed20f7014603d7e22c0c8c1
ralph <muyue15@qq.com>
2017-03-28 23:09:13 -0500

    Implement strStr() added

--- 028.Implement strStr()/SearchStr.java
+++ 028.Implement strStr()/SearchStr.java
@@ -2,8 +2,22 @@ package ralph;
 
 public class SearchStr {
 	
-    public int strStr(String haystack, String needle) {
-        
+    public static int strStr(String haystack, String needle) {
+
+	/** In order to find the needle in haystack, we need to make sure every char 
+    	 *  in needle matches a char in haystack, in certain sequence. Algorithm:
+	 *  1. Iterate through chars in haystack;
+	 *  2. Set two pointers, one at the beginning of needle, another at a certain
+	 *     position in haystack, compare the two chars at two pointers. 
+	 *  3. If chars are the same, move both pointers to next postions.
+	 *  4. If first pointer reaches the end of needle, return the start position
+	 *     of second pointer; if second pointer reaches the end of haystack,
+	 *     return -1;
+	 *  5. After iteration through haystack, return -1;
+	 *  6. Special case: if needle is a empty string, always return 0.
+	 *  The algorithm roughly compares word in every position in haystack. Assume
+	 *  haystack.length()=n, needle.length()=m, the algorithm runs O(n*m).
+    	 */ 
     	
     	if (needle.equals("")) return 0;
         
@@ -25,9 +39,12 @@ public class SearchStr {
 	
     
     
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+	String n1="def";
+	String h1="abcdef";
+		
+	System.out.printf("The starting position of %s in %s is %d\n",n1,h1,strStr(h1,n1));
+	System.out.printf("The starting position of %s in %s is %d\n",h1,n1,strStr(n1,h1));
+    }
 
 }
